e00fa3a16d7b680b482feeb2b3861afcf9869b71
Jetdoc345 <jw600345@gmail.com>
2024-10-06 14:10:23 -0400

    first commit

---
+++ Funny Story Assignment/funny story.js
@@ -0,0 +1,18 @@
+// 1. Declare Variables
+// Using let
+
+
+let character = "Rachel";
+let action = "ran";
+let object = "a kite";
+let place = "the park";
+let time = "in the afternoon during a tornado";
+
+//Using "const"
+const occasion = "while surfing";
+
+// 2. Create the story
+console.log("----Funny Story----");
+console.log(character + "was at"+ place + "on" + time + ".");
+console.log("suddenly " + character + "saw" + object + " and decided to " + action + ".");
+console.log("It turned out to be" + occasion + " and everyone had a great time!);
